175907eafe08d2f17088b96ac8cba4107f75ef17
julianagermano <57567229+julianagermano@users.noreply.github.com>
2026-03-23 10:09:01 -0300

    Update playwright.config.js

--- ui-api-token/playwright.config.js
+++ ui-api-token/playwright.config.js
@@ -8,7 +8,7 @@ module.exports = defineConfig({
     ['list'],
     ['html',  { outputFolder: 'playwright-report', open: 'never' }],
     ['junit', { outputFile: 'test-results/junit.xml' }]
-  ],
+  ]
   use: {
     headless: true,
     trace: 'on-first-retry',
