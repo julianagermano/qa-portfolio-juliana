3d94f1db6c23c09b188e2d83322a97f1f72a9118
julianagermano <57567229+julianagermano@users.noreply.github.com>
2026-03-23 15:05:13 -0300

    Update playwright.config.js

--- ui-api-token/playwright.config.js
+++ ui-api-token/playwright.config.js
@@ -1,14 +1,14 @@
-// Config Playwright (CommonJS) — limpa para CI
+// ui-api-token/playwright.config.js
 const { defineConfig } = require('@playwright/test');
 
 module.exports = defineConfig({
   testDir: './tests',
   timeout: 60_000,
-reporter: [
-  ['list'],
-  ['html',  { outputFolder: 'playwright-report', open: 'never' }],
-  ['junit', { outputFile: 'test-results/junit.xml' }]
-]
+  reporter: [
+    ['list'],
+    ['html',  { outputFolder: 'playwright-report', open: 'never' }],
+    ['junit', { outputFile: 'test-results/junit.xml' }]
+  ],
   use: {
     headless: true,
     trace: 'on-first-retry',
