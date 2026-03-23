53650e8e2ce5a98cd41c9ab0a33e6782dbbdbc24
julianagermano <57567229+julianagermano@users.noreply.github.com>
2026-03-23 10:57:49 -0300

    Update playwright.config.js

--- ui-api-token/playwright.config.js
+++ ui-api-token/playwright.config.js
@@ -1,16 +1,14 @@
-// Config CJS estável para CI (Playwright)
+// Configuração Playwright (CommonJS) — estável para CI no Azure DevOps
 const { defineConfig } = require('@playwright/test');
 
 module.exports = defineConfig({
   testDir: './tests',
   timeout: 60_000,
-
-reporter: [
-  ['list'],
-  ['html',  { outputFolder: 'playwright-report', open: 'never' }],
-  ['junit', { outputFile: 'test-results/junit.xml' }]
-]
-  
+  reporter: [
+    ['list'],
+    ['html',  { outputFolder: 'playwright-report', open: 'never' }],
+    ['junit', { outputFile: 'test-results/junit.xml' }]
+  ],
   use: {
     headless: true,
     trace: 'on-first-retry',
